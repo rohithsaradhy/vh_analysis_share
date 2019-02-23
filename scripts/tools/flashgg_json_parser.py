6307d4d3687796b438a1a2cc9db881439d12662e
Rohith Saradhy <rohithsaradhy@gmail.com>
2019-02-23 00:41:33 -0600

    generated a json which is merged between 3.1 and 3.2; Made a method to do this and fixed a bug

--- scripts/tools/flashgg_json_parser.py
+++ scripts/tools/flashgg_json_parser.py
@@ -1,13 +1,29 @@
 #Written by Rohith Saradhy
 # Description:
 #  Looks at all the *json files in the root_folder and creates a json file of processes given in the listOfInterest list
-#  saves the final process json into a filed named by the variable outputName; default is output.json
+#  saves the final process json into a file named by the variable outputName if saveFile is set to True; default is output.json
 #  
 #Need the following libraries for the function to work:
 import json
 import glob
 
-def findDataFromCatalog(root_folder, listOfInterest,outputName="output.json",): 
+def MergeJSON(d1,d2,campaign=None):
+    if campaign != None:
+        d1['cmdLine']="campaign="+campaign
+    for item in d1['processes'].keys():
+        for entries in d2['processes'][item]:
+            d1['processes'][item].append(entries)
+    return d1;
+
+def saveJSON(outputName,data_out):
+    with open(outputName,'w') as fp:
+        print "Saving to the following file: " + outputName
+        json.dump(data_out,fp,sort_keys=True,indent=4)
+        print "Done Saving"
+    return None
+    
+
+def findDataFromCatalog(root_folder, listOfInterest,outputName="output.json",saveFile=False): 
     list_files = glob.glob(root_folder+"/*json")
     if len(list_files) < 1:
         print "The folder does not contain any files...; Please Check Again"
@@ -28,16 +44,16 @@ def findDataFromCatalog(root_folder, listOfInterest,outputName="output.json",):
         for key in data:
             # pprint(data[key].keys())
             for names in data[key].keys():
-                if listOfInterest[int(lookingAt)] in names:
+                if ('/'+listOfInterest[int(lookingAt)]) in names: #the / in front helps to isolate channels beginning
 #                     print listOfInterest[int(lookingAt)]+'\t'+key + "\n" + names + "\n"
                     data_process[listOfInterest[int(lookingAt)]].append(names)
                     
     data_out['processes'] = data_process
+    if saveFile:
+        saveJSON(outputName,data_out)
+        return None
+    else:
+        return data_out
     
-    with open(outputName,'w') as fp:
-        print "Saving to the following file: " + outputName
-        json.dump(data_out,fp,sort_keys=True,indent=4)
-        print "Done Saving"
-    return None
-
+ 
 
